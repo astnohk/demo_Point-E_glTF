7f7c37e39dca2734f760a1230d9218a7b8e0267e
astnohk <kon.bass@gmail.com>
2023-01-08 18:54:35 +0900

    Add `--prompt` and `--output` argument option
    * `--prompt` specify user intended prompt text.
    * `--output` specify output filepath.

--- txt2XYZ.py
+++ txt2XYZ.py
@@ -3,13 +3,18 @@ from util import xyz_util
 
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prompt', help='Prompt', type=str, default='a red motorcycle')
+    parser.add_argument('--output', help='Output XYZ filepath (*.xyz)', type=str, default='pc.xyz')
+    args = parser.parse_args()
+
     point_e = txt2mesh.point_e_wrapper(guidance_scale=3.0)
     # Set a prompt to condition on.
-    prompt = 'a red motorcycle'
 
-    pc = point_e.sample(prompt)
+    pc = point_e.sample(args.prompt)
 
     # Write the mesh to a PLY file to import into some other program.
-    with open('pc.xyz', 'w') as f:
+    with open(args.output, 'w') as f:
         xyz_util.write_xyz(f, pc)
 
--- txt2glTF.py
+++ txt2glTF.py
@@ -4,13 +4,18 @@ from util import gltf_util
 
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--prompt', help='Prompt', type=str, default='a red motorcycle')
+    parser.add_argument('--output', help='Output glTF filepath (*.glb or *.gltf)', type=str, default='mesh.glb')
+    args = parser.parse_args()
+
     point_e = txt2mesh.point_e_wrapper(guidance_scale=3.0)
     # Set a prompt to condition on.
-    prompt = 'a red motorcycle'
 
-    pc = point_e.sample(prompt)
+    pc = point_e.sample(args.prompt)
     mesh = point_e.get_mesh(pc)
 
     # Write the mesh to a PLY file to import into some other program.
-    gltf_util.write_gltf('mesh.glb', mesh)
+    gltf_util.write_gltf(args.output, mesh)
 
